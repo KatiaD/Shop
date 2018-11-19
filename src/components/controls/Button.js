7397e86314e88b62f008a203d25bcb572ff34d38
Kateryna Derevenko <kderevenko@gs20020-macpro.on.seo>
2018-11-19 19:07:14 +0200

    inputs, selectors, buttons

--- src/components/controls/Button.js
+++ src/components/controls/Button.js
@@ -7,6 +7,16 @@ const Button = styled.button.attrs({
   color: ${ ({theme: {colorAccent}}) => colorAccent };
   background-color: ${ ({theme: {colorDefault}}) => colorDefault };
   font-size: ${ ({theme: {fontSize}}) => fontSize + 'px' };
+  width: ${ ({theme: {width}}) => width + 55 + 'px' };
+  height: ${ ({theme: {height}}) => height + 5 + 'px' };
+  :hover {
+    color: ${ ({theme: {colorDefault}}) => colorDefault };
+    background-color: ${ ({theme: {colorAccent}}) => colorAccent };
+    border: 1px solid  ${ ({theme: {colorDefault}}) => colorDefault };
+  }
+  :focus {
+    outline: none;
+  }
 `;
 
 export default Button;
--- src/components/controls/input.jsx
+++ src/components/controls/input.jsx
@@ -6,6 +6,28 @@ const Input = styled.input.attrs({
 })`
   color: ${ ({theme: {colorDefault}}) => colorDefault };
   font-size: ${ ({theme: {fontSize}}) => fontSize + 'px' };
+  margin-top: 25px;
+  height: 30px;
+  width: 360px;
+  background: none;
+  border: none;
+  border-bottom: 1px solid  ${ ({theme: {colorDefault}}) => colorDefault };
+  font-size: 18px;
+  transition: line-height 0.5s ease;
+  @media (max-width: 414px) {
+    width: 310px;
+  }; 
+  :focus::-webkit-input-placeholder {
+    color: transparent;
+    outline: none;
+  }
+  :focus:-ms-input-placeholder {
+    color: transparent;
+    outline: none;
+  }
+  :focus {
+    outline: none;
+  }
 `;
 
 export default Input;
